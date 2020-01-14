b89c68df72bb5b59031b3063bf6abc80759c791c
Breno Lucas da Silva Santos <brenolucas2015@outlook.com>
2020-01-14 14:35:56 -0300

    Primeiro commit de verdade

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:noderest', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:noderest', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 mongoose.Promise = global.Promise;
 
--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,11 +1,32 @@
+const path = require('path');
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 
-const { host, post, user, pass } = require('../config/mail');
+const { host, port, user, pass } = require('../config/mail');
 
 const transport = nodemailer.createTransport({
     host,
     port,
     auth: { user, pass }
 });
+
+const handlebarOptions = {
+    viewEngine: {
+        extName: '.hbs',
+        partialsDir: path.resolve('./src/resources/mail/auth'),
+        layoutsDir: path.resolve('./src/resources/mail/auth'),
+        defaultLayout: 'forgot_password.html'
+    },
+    viewPath: path.resolve('./src/resources/mail/'),
+    extName: '.html',
+};
+
+transport.use('compile', hbs(handlebarOptions));
+
+// transport.use('compile', hbs({
+//     viewEngine: 'handlebars',
+//     viewPath: path.resolve('./src/resources/mail/'),
+//     extName: '.html'
+// }));
 
 module.exports = transport;
